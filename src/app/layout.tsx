928b51f1ebda7b67b4df2953fd7351e35981619a
Mykel68 <olowookeremicheal9568@gmail.com>
2025-04-17 17:54:52 +0100

    feat:configuring tanstack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import ReactQueryProvider from '@/providers/ReactQueryProvider';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -24,10 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body        className={`${geistSans.variable} ${geistMono.variable} antialiased`}      >
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
