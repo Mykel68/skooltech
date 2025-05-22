d23d88427b29c4cf84d0ffd14e528b7d9d7f00f4
Mykel68 <olowookeremicheal9568@gmail.com>
2025-05-22 11:57:13 +0100

    fix: standardize import statements and enhance QueryClient initialization in ReactQueryProvider

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,15 +1,22 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode } from "react";
 
 export default function ReactQueryProvider({
   children,
 }: {
   children: ReactNode;
 }) {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // false for now on development
+        staleTime: 10 * 60 * 1000, // 10 minutes
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
