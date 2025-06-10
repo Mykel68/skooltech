c9f5a0a7a95fbbb122e546ae1f519108c4e62a03
Mykel68 <olowookeremicheal9568@gmail.com>
2025-06-10 13:03:04 +0100

    login pge

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,28 +1,28 @@
-import type React from "react";
+import type React from 'react';
 // import { Sidebar } from "@/components/sidebar";
-import { SidebarProvider } from "@/components/sidebar-provider";
-import { Header } from "@/components/header";
-import { Sidebar } from "@/components/sidebar";
+import { SidebarProvider } from '@/components/sidebar-provider';
+import { Header } from '@/components/Header';
+import { Sidebar } from '@/components/sidebar';
 // import ClientLayout from "./client-layout";
 
 export default function DashboardLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <SidebarProvider>
-      <div className="min-h-screen bg-background">
-        <Sidebar />
-        <div className="lg:pl-72">
-          <Header />
-          <main className="p-4 md:p-6 lg:p-8">
-            {/* <ClientLayout> */}
-            {children}
-            {/* </ClientLayout> */}
-          </main>
-        </div>
-      </div>
-    </SidebarProvider>
-  );
+	return (
+		<SidebarProvider>
+			<div className='min-h-screen bg-background'>
+				<Sidebar />
+				<div className='lg:pl-72'>
+					<Header />
+					<main className='p-4 md:p-6 lg:p-8'>
+						{/* <ClientLayout> */}
+						{children}
+						{/* </ClientLayout> */}
+					</main>
+				</div>
+			</div>
+		</SidebarProvider>
+	);
 }
