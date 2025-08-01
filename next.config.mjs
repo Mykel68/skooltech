f30517d9a197eefb13f65bfa95fcbfec17c6678a
Mplus <olowookeremicheal9568@gmail.com>
2025-08-01 13:50:52 +0100

    chore:checking for offline

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,22 @@ const withPWA = withPWAInit({
   workboxOptions: {
     disableDevLogs: true,
   },
+  runtimeCaching: [
+    {
+      urlPattern: /^\/api\/.*/, // Match all API routes
+      handler: "StaleWhileRevalidate", // Serve cache offline, update in background
+      options: {
+        cacheName: "api-cache",
+        expiration: {
+          // maxEntries:  // Limit cache size (adjust as needed)
+          maxAgeSeconds: 1.5 * 24 * 60 * 60, // Cache for 1.5 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200], // Cache successful responses
+        },
+      },
+    },
+  ],
 });
 
 /** @type {import('next').NextConfig} */
